0311c305d651937ed900deda930b6f7c1ace096c
Cloud <cloud@cloudy.gay>
2021-12-14 13:46:47 -0800

    removed unneccessary comments

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,3 @@
-// @ts-check // Can be removed, used to check typings and valid actions
-const { MessageEmbed } = require('discord.js');
-
 module.exports = {
   name: 'ping',
   description: 'Get bot ping.',
