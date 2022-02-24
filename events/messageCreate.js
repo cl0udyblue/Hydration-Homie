e63f5fefaaf50bf0791313d5c7ee1560fa1f4ffc
Cloud <78389093+froggiecloud@users.noreply.github.com>
2022-02-24 15:16:33 -0800

    set up boop in support.froggie.cc

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,11 @@
     module.exports = async (client, message) => {
+        if(message.channel.id === "941184367315812372" && message.content.toLowerCase() !== "boop") {
+            try{
+            message.delete()
+        } catch(err) {
+            console.log(err)
+        }
+        }
     if (message.author.bot) return;
     if (message.content.indexOf(client.config.prefix) !== 0) return;
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
